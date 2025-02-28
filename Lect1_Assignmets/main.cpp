669900394a2519d176666958280736711a3b6cd9
23021414-CTDLGT <23021414@vnu.edu.vn>
2025-02-28 23:26:29 +0700

    Add files via upload

--- Lect1_Assignmets/main.cpp
+++ Lect1_Assignmets/main.cpp
@@ -1,45 +1,20 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
-
-class Point{
-public:
-    double x, y;
-    void import(){
-        cin >> x >> y;
-    }
-};
-class Line { // y = ax + b
-public:
-    double a, b;
-    Line ( Point P1, Point P2){
-        a=(P1.y-P2.y)/(P1.x-P2.x);
-        b=(P1.y-P1.x*a);
-    }
-};
-void findIntersectionPoint (Line L1, Line L2){
-    if (L1.a==L2.a&&L1.b==L2.b){
-        cout<< "MANY";
-    }else if(L1.a==L2.a&&L1.b!=L2.b){
-        cout <<"NO";
-    }else{
-        double x,y;
-        x=(L1.b-L2.b)/(L2.a-L1.a);
-        y=(L1.a*x-L1.b);
-        printf("%.2f %.2f",x,y);
-    }
+int uoc_max(int a, int b)
+{
+	int n = a % b;
+	if (n == 0)
+	{
+		return b;
+	}
+	else {
+		uoc_max(b, n);
+	}
 }
-int main(){
-    Point A, B, C, D;
-    A.import();
-    B.import();
-    C.import();
-    D.import();
-
-    Line AB(A,B);
-    Line CD(C,D);
-
-    findIntersectionPoint(AB, CD);
-
-    return 0;
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << uoc_max(a, b);
+	return 0;
 }
